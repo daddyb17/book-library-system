a550c229af80686dd1366f23a0d5867751a269f0
830-G5\HP <abiodunvictor11@gmail.com>
2026-03-21 17:41:04 +0800

    Initialize Spring Boot book library system project

---
+++ src/main/java/com/collabera/booklibrarysystem/BooklibrarysystemApplication.java
@@ -0,0 +1,13 @@
+package com.collabera.booklibrarysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklibrarysystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooklibrarysystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collabera/booklibrarysystem/BooklibrarysystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collabera.booklibrarysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklibrarysystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
